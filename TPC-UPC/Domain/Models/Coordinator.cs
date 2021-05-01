6c7ce3f30cf3dd8feb6b2a0df66780df670b8748
Caro2870 <carol.vi28@gmail.com>
2021-05-01 06:14:01 -0500

    Revisar entidades

---
+++ TPC-UPC/Domain/Models/Coordinator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TPC_UPC.Domain.Models
+{
+    public class Coordinator
+    {
+    }
+}
--- TPC-UPC/Domain/Models/University.cs
+++ TPC-UPC/Domain/Models/University.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace TPC_UPC.Domain.Models
 {
-    public class Universities
+    public class University
     {
     }
 }
