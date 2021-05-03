7d2945b274e50b3ca73a25cce223f6ba6b8d3008
Lucas Alejandro Moreno Olivos <lucas.moreno.olivos@gmail.com>
2021-05-03 11:14:57 -0500

    Revert "Revert "Models Added""
    This reverts commit 2d5439236926f3a661d6ac487be7d0c931dac15e.

--- TPC-UPC/Domain/Models/Course.cs
+++ TPC-UPC/Domain/Models/Course.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Web.Domain.Models
+namespace TPC_UPC.Domain.Models
 {
     public class Course
     {
         public int Id { get; set; }
-        public String Name { get; set; }
+        public string CourseName { get; set; }
 
-        public List<UserCourse> UserCourses { get; set; }
+        public List<UserCourse> UserCouerses { get; set; }
     }
 }
