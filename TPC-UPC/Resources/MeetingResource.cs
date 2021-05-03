9828691c225933694b9709dee8c7ffd00aebcf6e
Lucas <lucas.moreno.olivos@gmail.com>
2021-05-03 04:39:15 -0500

    ResourceToModel finish

--- TPC-UPC/Resources/MeetingResource.cs
+++ TPC-UPC/Resources/MeetingResource.cs
@@ -8,7 +8,7 @@ namespace TPC_UPC.Resources
     public abstract class MeetingResource
     {
         public int Id { get; set; }
-        public SheduleResource Shedule { get; set; }
+        public ScheduleResource Shedule { get; set; }
         public string Description { get; set; }
         public string MeetingLink { get; set; }
         public string ResourceLink { get; set; }
