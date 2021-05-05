d7add78303406ca3a1c639e5b634ce0e953f78f8
Brigitte <u201914955@upc.edu.pe>
2021-05-05 12:45:17 -0500

    UniversityController

--- TPC-UPC/Controllers/AccountController.cs
+++ TPC-UPC/Controllers/AccountController.cs
@@ -4,7 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TPC_UPC.Domain.Services;
-
+using AutoMapper;
+using Swashbuckle.AspNetCore.Annotations;
+using TPC_UPC.Domain.Services;
+using TPC_UPC.Resources;
+using TPC_UPC.Domain.Models;
+using TPC_UPC.Extensions;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TPC_UPC.Controllers
