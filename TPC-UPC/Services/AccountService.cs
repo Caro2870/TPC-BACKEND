386790128095fb59987f58d18c7275725b2f009f
JPonte09 <u201913771@upc.edu.pe>
2021-05-05 05:32:31 -0500

    Commit de Schedule and Meeting Service

---
+++ TPC-UPC/Services/AccountService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPC_UPC.Domain.Models;
+using TPC_UPC.Domain.Services;
+using TPC_UPC.Domain.Services.Communications;
+
+namespace TPC_UPC.Services
+{
+    public class AccountService : IAccountService
+    {
+        public Task<AccountResponse> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccountResponse> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Account>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Account>> ListByUniversityIdAsync(int universityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccountResponse> SaveAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccountResponse> UpdateASync(int id, Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TPC-UPC/Services/LessonService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPC_UPC.Domain.Services;
+
+namespace TPC_UPC.Services
+{
+    public class LessonService : ILessonService
+    {
+    }
+}
