578425fe3d438b75ce06e24383f93b5df0e5016e
JPonte09 <u201913771@upc.edu.pe>
2021-05-05 17:20:40 -0500

    Controller de Lesson, LessonType y LessonStudent

--- TPC-UPC/Controllers/LessonStudentController.cs
+++ TPC-UPC/Controllers/LessonStudentController.cs
@@ -1,15 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TPC_UPC.API.Extensions;
+using TPC_UPC.Domain.Models;
+using TPC_UPC.Domain.Services;
+using TPC_UPC.Resources;
 
 namespace TPC_UPC.Controllers
 {
-    [ApiController]
     [Route("/api/[controller]")]
-    public class LessonStudentController : ControllerBase
+    [Produces("application/json")]
+    [ApiController]
+    class LessonStudentController : ControllerBase
     {
+        private readonly ILessonStudentService _lessonStudentService;
+        private readonly IMapper _mapper;
+
+        public LessonStudentController(ILessonStudentService lessonStudentService, IMapper mapper)
+        {
+            _lessonStudentService = lessonStudentService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(LessonStudentResource), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var result = await _lessonStudentService.GetByIdAsync(id);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var lessonStudentResource = _mapper.Map<LessonStudent, LessonStudentResource>(result.Resource);//de Entity a Resource
+            return Ok(lessonStudentResource);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(LessonStudentResource), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IActionResult> PostAsync([FromBody] SaveLessonStudentResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var lessonStudent = _mapper.Map<SaveLessonStudentResource, LessonStudent>(resource);
+            var result = await _lessonStudentService.SaveAsync(lessonStudent);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var lessonStudentResource = _mapper.Map<LessonStudent, LessonStudentResource>(result.Resource);
+            return Ok(lessonStudentResource);
+        }
     }
 }
