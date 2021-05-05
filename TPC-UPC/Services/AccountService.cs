25c818c7a5b91310e904decaba87819a47b7f570
Brigitte <u201914955@upc.edu.pe>
2021-05-05 10:34:42 -0500

    2nd implementation, update in AccountService

--- TPC-UPC/Services/AccountService.cs
+++ TPC-UPC/Services/AccountService.cs
@@ -11,6 +11,14 @@ namespace TPC_UPC.Services
 {
     public class AccountService : IAccountService
     {
+        private readonly IAccountRepository _accountRepository;
+        private IUnitOfWork _unitOfWork;
+        public AccountService(IAccountRepository object1, IUnitOfWork object2)
+        {
+            this._accountRepository = object1;
+            this._unitOfWork = object2;
+        }
+
         public Task<AccountResponse> DeleteAsync(int id)
         {
             throw new NotImplementedException();
@@ -33,12 +41,36 @@ namespace TPC_UPC.Services
 
         public Task<AccountResponse> SaveAsync(Account account)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _accountRepository.AddAsync(account);
+                await _unitOfWork.CompleteAsync();
+                return new AccountResponse(account);
+            }
+            catch (Exception e)
+            {
+                return new AccountResponse($"An error ocurred while saving {e.Message}");
+            }
         }
 
         public Task<AccountResponse> UpdateASync(int id, Account account)
         {
-            throw new NotImplementedException();
+            var existingAccount = await _accountRepository.FindById(id);
+            if (existingAccount == null)
+            {
+                return new AccountResponse("Account not found");
+            }
+            existingAccount.Password = account.Password;
+            try
+            {
+                _accountRepository.Update(existingAccount);
+                await _unitOfWork.CompleteAsync();
+                return new AccountResponse(existingAccount);
+            }
+            catch (Exception e)
+            {
+                return new AccountResponse($"An error ocurred while updating the account {e.Message} ");
+            }
         }
     }
 }
