69c1d0eddc44510e3e6c94386b3cb5899efbde4e
JPonte09 <u201913771@upc.edu.pe>
2021-05-05 15:52:08 -0500

    Avance de Controllers de Schedule y Meeting

---
+++ TPC-UPC/Controllers/MeetingController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPC_UPC.Domain.Models;
+using TPC_UPC.Resources;
+using TPC_UPC.Services;
+
+namespace TPC_UPC.Controllers
+{
+    [Route("/api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    class MeetingController : ControllerBase
+    {
+        private readonly MeetingService _meetingService;
+        private readonly IMapper _mapper;
+
+        public MeetingController(MeetingService meetingService, IMapper mapper)
+        {
+            _meetingService = meetingService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(MeetingResource), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var result = await _meetingService.GetByIdAsync(id);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var meetingResource = _mapper.Map<Meeting, MeetingResource>(result.Resource);//de Entity a Resource
+            return Ok(meetingResource);
+        }
+    }
+}
---
+++ TPC-UPC/Controllers/ScheduleController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPC_UPC.Domain.Models;
+using TPC_UPC.Resources;
+using TPC_UPC.Services;
+
+namespace TPC_UPC.Controllers
+{
+    [Route("/api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly ScheduleService _scheduleService;
+        private readonly IMapper _mapper;
+
+        public ScheduleController(ScheduleService scheduleService, IMapper mapper)
+        {
+            _scheduleService = scheduleService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ScheduleResource), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var result = await _scheduleService.GetByIdAsync(id);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var scheduleResource = _mapper.Map<Schedule, ScheduleResource>(result.Resource);//de Entity a Resource
+            return Ok(scheduleResource);
+        }
+    }
+}
