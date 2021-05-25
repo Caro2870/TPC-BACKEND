d3269ec33d63102aa0ce9eb8912894779a3890b9
JPonte09 <u201913771@upc.edu.pe>
2021-05-25 13:58:28 -0500

    Training completed

---
+++ TPC-UPC/Controllers/TrainingsController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Swashbuckle.AspNetCore.Annotations;
+using TPC_UPC.Domain.Services;
+using TPC_UPC.Resources;
+using TPC_UPC.Domain.Models;
+using TPC_UPC.API.Extensions;
+
+namespace TPC_UPC.Controllers
+{
+    [Route("/api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class TrainingsController : ControllerBase
+    {
+        private readonly ITrainingService _trainingService;
+        private readonly IMapper _mapper;
+
+        public TrainingsController(ITrainingService trainingService, IMapper mapper)
+        {
+            _trainingService = trainingService;
+            _mapper = mapper;
+        }
+
+        [SwaggerOperation(
+            Summary = "List all trainings",
+            Description = "List of trainings",
+            OperationId = "ListAllTrainings")]
+        [SwaggerResponse(200, "List of Trainings", typeof(IEnumerable<TrainningResource>))]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<TrainningResource>), 200)]
+        public async Task<IEnumerable<TrainningResource>> GetAllAsync()
+        {
+            var trainings = await _trainingService.ListAsync();
+            var resources = _mapper
+                .Map<IEnumerable<Training>, IEnumerable<TrainningResource>>(trainings);
+            return resources;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(TrainningResource), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var result = await _trainingService.GetByIdAsync(id);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var trainingResource = _mapper.Map<Training, TrainningResource>(result.Resource);
+            return Ok(trainingResource);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(TrainningResource), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IActionResult> PostAsync([FromBody] SaveTrainningResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var training = _mapper.Map<SaveTrainningResource, Training>(resource);
+            var result = await _trainingService.SaveAsync(training);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var trainingResource = _mapper.Map<Training, TrainningResource>(result.Resource);
+            return Ok(trainingResource);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(TrainningResource), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IActionResult> PutAsync(int id, [FromBody] SaveTrainningResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+
+            var training = _mapper.Map<SaveTrainningResource, Training>(resource);
+            var result = await _trainingService.UpdateAsync(id, training);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            var trainingResource = _mapper.Map<Training, TrainningResource>(result.Resource);
+            return Ok(trainingResource);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(TrainningResource), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var result = await _trainingService.DeleteAsync(id);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            var trainingResource = _mapper.Map<Training, TrainningResource>(result.Resource);
+            return Ok(trainingResource);
+        }
+    }
+}
---
+++ TPC-UPC/Services/TrainingService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TPC_UPC.Domain.Models;
+using TPC_UPC.Domain.Persistence.Repositories;
+using TPC_UPC.Domain.Services;
+using TPC_UPC.Domain.Services.Communications;
+
+namespace TPC_UPC.Services
+{
+    public class TrainingService : ITrainingService
+    {
+        private readonly ITrainingRepository _trainingRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TrainingService(ITrainingRepository trainingRepository, IUnitOfWork unitOfWork)
+        {
+            _trainingRepository = trainingRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        //CRUD
+        public async Task<TrainingResponse> SaveAsync(Training training)
+        {
+            try
+            {
+                await _trainingRepository.AddAsync(training);
+                await _unitOfWork.CompleteAsync();
+                return new TrainingResponse(training);
+            }
+            catch (Exception e)
+            {
+                return new TrainingResponse($"An error ocurred while saving {e.Message}");
+            }
+        }
+
+        public async Task<TrainingResponse> GetByIdAsync(int trainingId)
+        {
+            var existingTraining = await _trainingRepository.FindById(trainingId);
+
+            if (existingTraining == null)
+                return new TrainingResponse("Training not found");
+            return new TrainingResponse(existingTraining);
+        }
+
+        public async Task<TrainingResponse> UpdateAsync(int id, Training training)
+        {
+            var existingTraining = await _trainingRepository.FindById(id);
+
+            if (existingTraining == null)
+                return new TrainingResponse("Training not found");
+
+            existingTraining.CoordinatorId = training.CoordinatorId;
+
+            try
+            {
+                _trainingRepository.Update(existingTraining);
+                await _unitOfWork.CompleteAsync();
+
+                return new TrainingResponse(existingTraining);
+            }
+            catch (Exception ex)
+            {
+                return new TrainingResponse($"An error ocurred while updating the training: {ex.Message}");
+            }
+        }
+
+        public async Task<TrainingResponse> DeleteAsync(int id)
+        {
+            var existingTraining = await _trainingRepository.FindById(id);
+
+            if (existingTraining == null)
+                return new TrainingResponse("Training not found");
+
+            try
+            {
+                _trainingRepository.Remove(existingTraining);
+                await _unitOfWork.CompleteAsync();
+
+                return new TrainingResponse(existingTraining);
+            }
+            catch (Exception ex)
+            {
+                return new TrainingResponse($"An error ocurred while deleting the training: {ex.Message}");
+            }
+        }
+
+        //ADDED
+        public async Task<IEnumerable<Training>> ListAsync()
+        {
+            return await _trainingRepository.ListAsync();
+        }
+    }
+}
