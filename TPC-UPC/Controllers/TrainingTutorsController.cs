dd02c0cdaac77d9298923649b90b4151ca810bcc
JPonte09 <u201913771@upc.edu.pe>
2021-05-25 14:38:59 -0500

    Training Tutor completed

---
+++ TPC-UPC/Controllers/TrainingTutorsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TPC_UPC.Domain.Models;
+using TPC_UPC.Domain.Services;
+using TPC_UPC.Resources;
+
+namespace TPC_UPC.Controllers
+{
+    [Route("api/trainings/{trainingId}/tutors")]
+    [ApiController]
+    public class TrainingTutorsController : ControllerBase
+    {
+        private readonly ITrainingTutorService _trainingTutorService;
+        private readonly IMapper _mapper;
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<TrainningTutorResource>), 200)]
+        public async Task<IEnumerable<TrainningTutorResource>> GetAllAsync()
+        {
+            var trainingTutors = await _trainingTutorService.ListAsync();
+            var resources = _mapper
+                .Map<IEnumerable<TrainingTutor>, IEnumerable<TrainningTutorResource>>(trainingTutors);
+            return resources;
+        }
+    }
+}
---
+++ TPC-UPC/Domain/Services/ITrainingTutorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TPC_UPC.Domain.Models;
+
+namespace TPC_UPC.Domain.Services
+{
+    public interface ITrainingTutorService
+    {
+        Task<IEnumerable<TrainingTutor>> ListAsync();
+    }
+}
