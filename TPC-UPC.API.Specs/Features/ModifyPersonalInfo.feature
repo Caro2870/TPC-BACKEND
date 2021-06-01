54510be5e9cf336082019c660d1994737d96a5e1
Brigitte <u201914955@upc.edu.pe>
2021-06-01 16:41:43 -0500

    test: testing for business rule 0 functional

---
+++ TPC-UPC.API.Specs/Features/ModifyPersonalInfo.feature
@@ -0,0 +1,15 @@
+﻿Feature: ModifyPersonalInfo
+![ModifyPersonalInfo](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple test for updating the info of a tutor
+
+Link to a feature: [ModifyPersonalInfo](TPC-UPC.API.Specs/Features/ModifyPersonalInfo.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+Scenario: Update or check personal info
+	Given I create a new Tutor (<Id>, <FirstName>, <LastName>, <Mail>, <PhoneNumber>, <AccountId>, <FacultyId>)
+	When I Update the info of this Tutor (<SameId>,<NewFirstName>, <NewLastName>, <NewPhoneNumber>)
+	Then the updated Info should return <response>
+Examples: 
+| Id | FirstName | LastName | Mail         | PhoneNumber | AccountId | FacultyId | SameId | NewFirstName | NewLastName | NewPhoneNumber | response |
+| 1  | Renzo     | Diaz     | renzo@upc.pe | 998654846   | 1         | 1         |    1   | Alvaro       | Torres      | 992585684      | True     |
---
+++ TPC-UPC.API.Specs/Steps/UniversitySteps.cs
@@ -0,0 +1,68 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TPC_UPC.Domain.Models;
+using TPC_UPC.Domain.Persistence.Repositories;
+using TPC_UPC.Domain.Services;
+using TPC_UPC.Domain.Services.Communications;
+using TPC_UPC.Resources;
+using TPC_UPC.Services;
+
+namespace TPC_UPC.API.Specs.Steps
+{
+    [Binding]
+    public sealed class UniversitySteps
+    {
+        public readonly ScenarioContext _scenarioContext;
+        private University _university = new();
+        private Task<UniversityResponse> _response;
+
+        private static Mock<IUniversityRepository> GetDefaultIUniversityRepositoryInstance()
+        {
+            return new Mock<IUniversityRepository>();
+        }
+
+        private static Mock<IUnitOfWork> GetDefaultIUnitOfWorkInstance()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+
+        private IUniversityService _universityService = new UniversityService(GetDefaultIUniversityRepositoryInstance().Object, GetDefaultIUnitOfWorkInstance().Object);
+
+        public UniversitySteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"I  create  a  new  University  \((.*), (.*)\)")]
+        public void GivenICreateANewUniversity(string id, string universityName)
+        {
+            int realId = Int32.Parse(id);
+            _university.Id = realId;
+            _university.UniversityName = universityName;
+            _response = _universityService.SaveAsync(_university);
+        }
+
+        //[Given(@"I  create  a  new  University  (.*)")]
+        //public void GivenICreateANewUniversity(int id, string universityName)
+        //{
+        //    _university.UniversityName = universityName;
+
+        //    var mockUniversityRepository = GetDefaultIUniversityRepositoryInstance();
+        //    var mockIUnitOfWork = GetDefaultIUnitOfWorkInstance();
+        //    var mockUniversityService = new UniversityService(mockUniversityRepository.Object, mockIUnitOfWork.Object);
+        //    var mockUniversityController = new UniversitiesController(mockUniversityService, _mapper);
+
+        //    _result = mockUniversityController.PostAsync(_university);
+        //}
+
+        [Then(@"the  system  should  return  (.*)")]
+        public void ThenTheSystemShouldReturn(string responseCode)
+        {
+            string a = _response.Result.Success.ToString();
+            Assert.AreEqual(a, responseCode);
+        }
+    }
+}
